5da01242a82ac254bc6bf211278e305e8ea35385
GuyFromEkb <you@example.com>
2023-05-09 15:29:58 +0300

    добавил mongodb (mongoose)

--- backend/src/app/app.ts
+++ backend/src/app/app.ts
@@ -1,6 +1,7 @@
 import express, { Express, json } from 'express';
 import { Server } from 'http';
 import { AppControllers } from 'src/app/types';
+import mongoose from 'mongoose';
 
 export class App {
   app: Express;
@@ -22,7 +23,11 @@ export class App {
     this.app.use('/user', this.controllers.user);
   }
 
-  init = () => {
+  init = async () => {
+    await mongoose.connect(
+      'mongodb+srv://jwtauth:q1w2e3r4t5@jwt-auth.qn78np4.mongodb.net/?retryWrites=true&w=majority'
+    );
+
     this.useMiddleware();
     this.useRoutes();
     this.server = this.app.listen(this.port);
