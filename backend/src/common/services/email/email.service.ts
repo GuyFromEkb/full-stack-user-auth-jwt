7a5d873e35930589bcc2a59cd9882dc7d1b7ad4b
GuyFromEkb <guyFromEkb@example.com>
2023-05-11 23:49:52 +0300

    email service

--- backend/src/common/services/email/email.service.ts
+++ backend/src/common/services/email/email.service.ts
@@ -1,6 +1,40 @@
+import { logger } from '@common/Logger';
+import { TestAccount, Transporter, createTestAccount, createTransport, getTestMessageUrl } from 'nodemailer';
+
 class EmailServices {
+  private testAccount: TestAccount;
+  private transporter: Transporter;
+
+  constructor() {
+    this.init();
+  }
+
+  private init = async () => {
+    this.testAccount = await createTestAccount();
+
+    this.transporter = createTransport({
+      host: 'smtp.ethereal.email',
+      port: 587,
+      secure: false,
+      auth: {
+        user: this.testAccount.user,
+        pass: this.testAccount.pass,
+      },
+    });
+
+    logger.info('[EmailServices] OK');
+  };
+
   sendActivateLink = async (to: string, link: string) => {
-    console.log('SEND EMAIL TO: ', to, ' LINK: ', link);
+    const info = await this.transporter.sendMail({
+      from: 'foo@example.com',
+      to,
+      subject: 'Hello ✔',
+      text: '',
+      html: `<a href="http://localhost:7777/user/activate/${link}">Подтвердить регистрацию</a>`,
+    });
+
+    logger.info('Preview URL: %s', getTestMessageUrl(info));
   };
 }
 
