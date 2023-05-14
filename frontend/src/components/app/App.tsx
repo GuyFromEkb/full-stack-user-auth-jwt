70355a936923496cfa340ce1389432b74c418144
GuyFromEkb <you@example.com>
2023-05-14 13:06:45 +0300

    config ts-path on vite

--- frontend/src/components/app/App.tsx
+++ frontend/src/components/app/App.tsx
@@ -1,7 +1,38 @@
 import { FC } from "react"
-
-import "./App.css"
+import Button from "@mui/material/Button"
+import { Box, AppBar, Toolbar, IconButton, Typography, TextField, Container } from "@mui/material"
+import MenuIcon from "@mui/icons-material/Menu"
 
 export const App: FC = () => {
-  return <div> hello</div>
+  return (
+    <div>
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar position="static">
+          <Toolbar>
+            <IconButton size="large" edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }}>
+              <MenuIcon />
+            </IconButton>
+            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+              News
+            </Typography>
+            <Button color="inherit">Login</Button>
+          </Toolbar>
+        </AppBar>
+      </Box>
+      <Container maxWidth="sm">
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            gap: 2,
+            mt: 4,
+          }}
+        >
+          <TextField label="Email" variant="outlined" />
+          <TextField label="Password" variant="outlined" />
+          <Button variant="outlined">Login</Button>
+        </Box>
+      </Container>
+    </div>
+  )
 }
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,7 +2,7 @@ import React from "react"
 import ReactDOM from "react-dom/client"
 
 import "./styles/global.css"
-import { App } from "./components/app/App"
+import { App } from "@components/app/App"
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
