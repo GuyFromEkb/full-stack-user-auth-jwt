e649e43db41fc1dcc575bc2a4776ce09a92f39bb
GuyFromEkb <guyFromEkb@example.com>
2023-06-08 22:28:15 +0300

    docker для client

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -5,6 +5,15 @@ import tsconfigPaths from "vite-tsconfig-paths"
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths({ parseNative: true })],
+  //docker
+  server: {
+    host: true,
+    port: 3005,
+    watch: {
+      usePolling: true,
+    },
+  },
+  //@mui/material => Styled Components
   resolve: {
     alias: {
       "@mui/styled-engine": "@mui/styled-engine-sc",
